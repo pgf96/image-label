f73c8b241889bb6ef7ef7daaf90148721787beb6
pgf96 <pgfxx1@gmail.com>
2023-03-15 17:19:25 -0400

    refactor width/height for readability

--- src/pages/ImagePage.jsx
+++ src/pages/ImagePage.jsx
@@ -1,44 +1,22 @@
 import ImageLabeler from "../components/ImageLabeler/ImageLabeler"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import './ImagePage.css'
 import { useMediaQuery } from 'react-responsive'
-import { ParentSize, withScreenSize } from "@visx/responsive"
 import { useMemo } from "react"
 import { scaleLinear } from "@visx/scale"
 
 
 export default function ImagePage({ annotationData, setAnnotationData }) {
-
-
     const [savedAnnotation, setSavedAnnotation] = useState([])
-    // const [width, setWidth] = useState(window.innerWidth)
-
-    const imageSrc = "https://i.redd.it/vz16evolc7ka1.jpg"
 
     const largest = useMediaQuery({query: '(min-width: 1540px)'})
     const large = useMediaQuery({query: '(min-width: 1024px)'})
     const medium = useMediaQuery({query: '(min-width: 768px)'})
     const small = useMediaQuery({query: '(min-width: 480px)'})
 
-    const breakpoints = []
-
-    let width,height
-    if (largest) {
-        width = 900
-        height = 680
-    } else if (large) {
-        width = 850
-        height = 643
-    } else if (medium) {
-        width = 600
-        height = 454
-    } else if (small) {
-        width = 500
-        height = 378
-    } else {
-        width = 360
-        height = 272
-    }
+    const width = largest ? 900 : large ? 850 : medium ? 600 : small ? 500 : 360;
+    const height = largest ? 680 : large ? 643 : medium ? 454 : small ? 378 : 272;
+    
         
     const xScale = useMemo(
         () =>
