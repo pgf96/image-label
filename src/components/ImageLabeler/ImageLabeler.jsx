f7154b34f6d8e707c504ecd50615d3564dead64d
pgf96 <pgfxx1@gmail.com>
2023-03-15 16:59:15 -0400

    fix id on creation

--- src/components/ImageLabeler/ImageLabeler.jsx
+++ src/components/ImageLabeler/ImageLabeler.jsx
@@ -1,13 +1,14 @@
 import Cursor from "../Cursor/Cursor";
 import { useState, useEffect, useRef } from "react";
 import { EditableAnnotation, Label, Connector, CircleSubject } from "@visx/annotation";
+import { scaleLinear } from "@visx/scale";
+import { useMemo } from "react";
 
-export default function ImageLabeler({ annotationData, setAnnotationData, height, width, unsavedAnnotation, setUnsavedAnnotation }) {
+export default function ImageLabeler({ annotationData, setAnnotationData, height, width, xScale, yScale, savedAnnotation, setSavedAnnotation }) {
 
   const imageSrc = "https://i.redd.it/vz16evolc7ka1.jpg"
   const [isEditable, setIsEditable] = useState(false)
   const [clickCoordinates, setClickCoordinates] = useState([]);
-  // const [unsavedAnnotation, setUnsavedAnnotation] = useState([])
 
   const [newItem, setNewItem] = useState({
     title: '',
@@ -17,26 +18,25 @@ export default function ImageLabeler({ annotationData, setAnnotationData, height
     dy: 60,
     width: 150,
     height: 75,
-    id: Math.floor(Math.random() * 1000000)
+    id: 1
   })
-
 
   const inputRef = useRef(null)
   const svgRef = useRef(null)
+  const imageRef = useRef(null)
+  const labelRef = useRef(null)
 
   useEffect(function () {
     function getAnnotations() {
       const annotations = annotationData
-      setUnsavedAnnotation(annotations)
+      setSavedAnnotation(annotations)
     }
-    console.log('hi')
     getAnnotations()
   }, [])
 
 
-  function handleSubmit(e) {
+  function handleCreateItem(e) {
     e.preventDefault()
-    // setUnsavedAnnotation((prevData) => [...prevData, newItem]);
     setAnnotationData((prevData) => [...prevData, newItem]);
   }
 
@@ -46,6 +46,7 @@ export default function ImageLabeler({ annotationData, setAnnotationData, height
       title: e.target.value,
       x: clickCoordinates.x,
       y: clickCoordinates.y,
+      id: Math.floor(Math.random() * 1000000),
     }))
   }
 
@@ -57,35 +58,28 @@ export default function ImageLabeler({ annotationData, setAnnotationData, height
     );
   }
 
-  function handleAnnotationSave() {
-    // setAnnotationData(unsavedAnnotation)
-    setUnsavedAnnotation(annotationData)
+  function handleSaveAnnotations() {
+    setSavedAnnotation(annotationData)
   }
 
   function handleCheck() {
     setIsEditable(!isEditable)
     return isEditable
   }
-  const [scaleFactor, setScaleFactor] = useState(1);
-
-  useEffect(() => {
-    const newScaleFactor = window.innerWidth < 900 ? window.innerWidth / 900 : 1;
-    setScaleFactor(newScaleFactor);
-  }, [window.innerWidth]);
 
   return (
     <div>
-      <Cursor svgRef={svgRef} inputRef={inputRef} setClickCoordinates={setClickCoordinates} setAnnotationData={setAnnotationData} />
-      <div style={{ textAlign: 'left'}} >
+      <Cursor xScale={xScale} yScale={yScale} width={width} svgRef={svgRef} inputRef={inputRef} setClickCoordinates={setClickCoordinates} setAnnotationData={setAnnotationData} />
+      <div style={{ textAlign: 'left' }} >
         The mouse is at position{' '}
-        <b>({clickCoordinates.x}, {clickCoordinates.y})</b>
+        <b>({(clickCoordinates.x)}, {(clickCoordinates.y)})</b>
         <div>
-          <label for='edit'> edit </label>
+          <label htmlFor='edit'> edit </label>
           <input type="checkbox" name='edit' onChange={handleCheck} />
           <span>(allows you to move handles)</span>
         </div>
         <div>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleCreateItem}>
             <div>
               <input
                 type="text"
@@ -93,51 +87,57 @@ export default function ImageLabeler({ annotationData, setAnnotationData, height
                 onChange={handleChange}
                 ref={inputRef}
               />
-              <button type="submit">add to unsaved annotations</button>
+              <button type="submit">add to real time annotations</button>
             </div>
           </form>
         </div>
-      <button onClick={handleAnnotationSave}> save annotations </button>
+        <button onClick={handleSaveAnnotations}> save annotations </button>
       </div>
 
-      <div style={{ position: 'absolute' }}>
-        <img
-          src={imageSrc}
-          style={{ width: width, height: height }}
-        />
+      <div style={{ display: 'flex', margin: 'auto', position: 'relative', width: width, alignItems: "center", justifyContent: "center" }}>
         <svg
           ref={svgRef}
-          style={{
-            position: 'absolute',
-            left: '0px',
-            top: '0px',
-            width: width, height: height
-          }}>
+          width={width}
+          height={height}
+        >
+          <image ref={imageRef} href={imageSrc} width={width} />
           {/* {unsavedAnnotation.map((annotation) => ( */}
           {annotationData.map((annotation) => (
             <EditableAnnotation
               key={annotation.id}
-              x={annotation.x * scaleFactor}
-              y={annotation.y * scaleFactor}
-              dx={annotation.dx * scaleFactor}
-              dy={annotation.dy * scaleFactor}
-              width={annotation.width}
-              height={annotation.height}
+              // coordinates are displayed based on scaling factor
+              x={xScale(annotation.x)}
+              y={yScale(annotation.y)}
+              dx={xScale(annotation.dx)}
+              dy={yScale(annotation.dy)}
+              width={width}
+              height={height}
               canEditSubject={isEditable}
               canEditLabel={isEditable}
-              onDragEnd={(coord) => handleDragEnd(coord, annotation.id,)}>
+              onDragEnd={({ x, y, dx, dy }) => {
+                // coordinates are passed as unscaled in order to preserve the original coordinate data
+                const unscaledX = xScale.invert(x)
+                const unscaledY = yScale.invert(y)
+                const unscaledDx = xScale.invert(dx)
+                const unscaledDy = yScale.invert(dy)
+                handleDragEnd({ x: unscaledX, y: unscaledY, dx: unscaledDx, dy: unscaledDy }, annotation.id);
+              }}
+            >
               <Connector
-                stroke={'black'}
+                stroke={'red'}
                 type={'elbow'} />
               <CircleSubject
-                stroke={'black'}
+                stroke={'red'}
                 radius={3} />
               <Label
+                maxWidth={xScale(150)}
+                titleFontSize={xScale(16)}
                 title={annotation.title}
                 fontColor={'white'}
                 showBackground={true}
                 backgroundFill={'rgba(0, 0, 0, 0.3)'}
                 anchorLineStroke={'black'}
+                backgroundPadding={xScale(12)}
               />
             </EditableAnnotation>
           ))}
