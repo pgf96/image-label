8b19f18f50a7b13fafdd0f524d3b040da47455c5
pgf96 <pgfxx1@gmail.com>
2023-03-15 16:58:43 -0400

    add scaling to x,y mouse client

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -1,29 +1,27 @@
 import { useEffect } from "react";
 
-export default function Cursor({setClickCoordinates, inputRef, svgRef}) {
-    function handleMouseClick(e) {
-        // if the focused element is the input do nothing
-        if (document.activeElement === inputRef.current) {
-            return;
-          }
-        if (!svgRef.current.contains(e.target)) {
-            console.log('click outside')
-            return
-        } else {
-            console.log('click inside')
-            const imageRect = svgRef.current.getBoundingClientRect();
-            setClickCoordinates((prevState) => ({
-            ...prevState, 
-            x: e.clientX - imageRect.left,
-            y: e.clientY - imageRect.top 
-        }))
-        // last event of the click is to focus on input element
-        inputRef.current.focus()
-        }
-        
-    };
+export default function Cursor({ setClickCoordinates, inputRef, svgRef, xScale, yScale}) {
+  function handleMouseClick(e) {
+    // if the focused element is the input do nothing
+    if (document.activeElement === inputRef.current) {
+      return;
+    }
+    if (!svgRef.current.contains(e.target)) {
+      return
+    } else {
+      const imageRect = svgRef.current.getBoundingClientRect();
+      setClickCoordinates((prevState) => ({
+        ...prevState,
+        x: xScale.invert(e.clientX - imageRect.left),
+        y: yScale.invert(e.clientY - imageRect.top)
+      }))
+      // last event of the click is to focus on input element
+      console.log(xScale(1))
+      inputRef.current.focus()
+    }
+  };
 
-    useEffect(() => {
+  useEffect(() => {
     window.addEventListener('click', handleMouseClick);
 
     return () => {
@@ -32,8 +30,5 @@ export default function Cursor({setClickCoordinates, inputRef, svgRef}) {
         handleMouseClick
       );
     };
-  }, []);
-  return (
-    null
-  );
+  }, [xScale]);
 }
